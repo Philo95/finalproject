79c148733b94410383c71eee523c0dad942d4609
Philo95 <113151552+Philo95@users.noreply.github.com>
2022-12-12 03:27:28 -0500

    Add files via upload

---
+++ java/main.js
@@ -0,0 +1,7 @@
+
+  function addTheValue(secondValue) {
+    var fValue = document.getElementById("firstValue");
+    firstValue.innerHTML = parseInt(fValue.innerHTML) +
+    parseInt(secondValue);
+ }
+ 
